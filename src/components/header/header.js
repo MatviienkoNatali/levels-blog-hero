fefd881238fe3d27361a4c7cfb09365ac0986345
Nataliia_Matviienko <matviienko.natali@gmail.com>
2022-10-03 12:04:56 +0300

    added burger-menu

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,23 +1,32 @@
 import logo from "../../images/logo-levels.svg";
-import React from "react";
+import React, {useState} from "react";
 import * as style from "./header.module.scss";
 import {Link} from "gatsby";
 
 
 function Header() {
+    const [activeState, setActiveState] = useState(false)
     return (
         <header className={style.header}>
-            <div className="container">
-                <Link to={"./"}>
-                    <figure className="logo">
-                        <img src={logo} alt="logo"/>
-                    </figure>
-                </Link>
-                <div className="search"></div>
-                <ul className="menu">
-                    <li><Link to={"./"}>Levels Blog</Link></li>
-                    <li><Link to={"./"}>request access</Link></li>
-                </ul>
+            <div className={`wrap-header  ${activeState ? 'open' : ''}`}>
+                <div className={"container"}>
+                    <Link to={"./"}>
+                        <figure className="logo">
+                            <img src={logo} alt="logo"/>
+                        </figure>
+                    </Link>
+
+                    <div className="search"></div>
+                    <ul className={"menu"}>
+                        <li><Link to={"./"}>Levels Blog</Link></li>
+                        <li><Link to={"./"}>request access</Link></li>
+                    </ul>
+                    <button className="btn-menu" onClick={() => setActiveState(prev => !prev)}>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </button>
+                </div>
             </div>
         </header>
     )
