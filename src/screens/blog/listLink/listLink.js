94a3265803c8f9b2bc97dc3a2d07269d22b85950
Nataliia_Matviienko <matviienko.natali@gmail.com>
2022-10-03 15:36:44 +0300

    comment GrahpQL

--- src/screens/blog/listLink/listLink.js
+++ src/screens/blog/listLink/listLink.js
@@ -6,18 +6,18 @@ const ListLinks = ['All', 'mental health', 'metabolic basics', 'nutrition', 'per
 const createUrl = (item) => { return item.replace(/ /g,"_") }
 
 const ListLink = () => {
-    const data = useStaticQuery(graphql`
-    query {
-      allWpCategory {
-        nodes {
-          link
-          name
-        }
-      }
-    }
-  `);
+  //   const data = useStaticQuery(graphql`
+  //   query {
+  //     allWpCategory {
+  //       nodes {
+  //         link
+  //         name
+  //       }
+  //     }
+  //   }
+  // `);
 
-    const category = data?.allWpCategory.nodes;
+    // const category = data?.allWpCategory.nodes;
     const [active, setActive] = useState(null)
     useEffect(() => {
 
@@ -34,11 +34,11 @@ const ListLink = () => {
             <div className="container">
                 <div className="wrap-list">
                     <ul className="list">
-                        {category.map((item) => {
-                            item = item.name;
+                        {ListLinks.map((item, index) => {
+                            // item = item.name;
                             return (
                                 <Link to={"?category=" + createUrl(item)}
-                                      key={item.id}
+                                      key={index}
                                       onClick={() => setActive(createUrl(item))}
                                       className={`list-item ${active === createUrl(item) ? 'active' : ''} `}
                                 >
